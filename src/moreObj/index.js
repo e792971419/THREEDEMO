cc154dec1f72ac9782211e996da00978abb1dc74
xiasong <xiasong@uchonor.com>
2021-01-29 15:46:58 +0800

    移动端的好东西

--- src/moreObj/index.js
+++ src/moreObj/index.js
@@ -2,25 +2,73 @@
  * @Author: your name
  * @LastEditors: xiasong
  * @Date: 2021-01-28 12:51:06
- * @LastEditTime: 2021-01-28 18:45:29
+ * @LastEditTime: 2021-01-29 11:29:16
  * @Description: description
  * @FilePath: \THREEDEMO\src\moreObj\index.js
  */
 
-console.log(1);
-Promise.race([Promise.resolve(a), Promise.resolve(b)]).then(() => {
-  setTimeout(() => {
-    console.log(3);
-  }, 1500);
-});
+const fetchRequest = {
+  getOne: () => axios("./index.json"),
+  getTwo: () => axios("./index2.json"),
+};
 
-async function a() {
-  setTimeout(() => {
-    console.log(1);
-  }, 500);
-}
-async function b() {
-  setTimeout(() => {
-    console.log(2);
-  }, 1000);
-}
+// function num() {
+//   return new Promise((resolve, reject) => {
+//     axios("./index.json").then((res) => {
+//       resolve(res);
+//     });
+//   });
+// }
+
+// num().then((res) => {
+//   console.log(res);
+// });
+
+// axios.all([fetchRequest.getOne(), fetchRequest.getTwo()]).then((res) => {
+//   console.log(res, "axios");
+// });
+
+// Promise.all([fetchRequest.getOne(), fetchRequest.getTwo()]).then((res) => {
+//   console.log(res);
+// });
+// Promise.race([fetchRequest.getOne(), fetchRequest.getTwo()]).then((res) => {
+//   // 这里返回的是最快的那一个请求的数据，慢的请求还是会触发，但是目前没有找到 怎么拿数据
+//   console.log(res);
+// });
+
+// function getBothData() {
+//   return new Promise((resolve, reject) => {
+//     axios.all([fetchRequest.getOne(), fetchRequest.getTwo()]).then((res) => {
+//       resolve(res);
+//     });
+//   });
+// }
+
+// getBothData().then((res) => {
+//   console.log(res);
+//   data = res;
+// });
+
+// 将数据分开
+// getBothData().then(
+//   axios.spread((res1, res2) => {
+//     console.log(res1);
+//     console.log(res2);
+//   })
+// );
+
+const read = function (link) {
+  return new Promise((resolve, reject) => {
+    axios(link).then((res) => {
+      resolve(res);
+    });
+  });
+};
+
+const asyncRead = async function () {
+  const one = await read("./index.json");
+  const two = await read("./index2.json");
+  // console.log(one);
+  // console.log(two);
+  return [one, two];
+};
